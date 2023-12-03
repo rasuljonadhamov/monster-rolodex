ec428286faa7dc17c4b6e6a0ab38fc61e4642c7c
rasuljonadhamov <rasuljonadhamov@gmail.com>
2023-12-03 13:32:16 +0300

    Add styles

--- src/components/card-list/Cart-list.jsx
+++ src/components/card-list/Cart-list.jsx
@@ -1,13 +1,23 @@
 import { Component } from "react";
+import "./Cart-list.css";
 
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
 
     return (
-      <div>
+      <div className="card-list">
         {monsters.map((monster) => {
-          return <h1 key={monster.id}>{monster.name}</h1>;
+          return (
+            <div className="card-container" key={monster.id}>
+              <img
+                src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+                alt="Monster"
+              />
+              <h1>{monster.name}</h1>
+              <p>{monster.email}</p>
+            </div>
+          );
         })}
       </div>
     );
--- src/components/search/Search-bar.jsx
+++ src/components/search/Search-bar.jsx
@@ -1,11 +1,12 @@
 import { Component } from "react";
+import "./Search-box.css";
 
 class SearchBox extends Component {
   render() {
     return (
       <div>
         <input
-        type="search"
+          type="search"
           className={this.props.className}
           placeholder={this.props.placeholder}
           onChange={this.props.onChangeHanler}
